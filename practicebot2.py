1692ccd066ca7eae2a263c42d1bbdb277d06c012
Luke Melander <56447791+lukemelander@users.noreply.github.com>
2021-06-28 21:50:56 -0500

    Update practicebot2.py

--- practicebot2.py
+++ practicebot2.py
@@ -5,6 +5,8 @@ import requests
 
 url = "https://api.syncretism.io/ops"
 userin = '{"itm":true,"otm":false,"max-diff":30,"calls":true,"order-by":"iv_desc","stock":true,"limit":1}'
+#More parameters for user input are at https://ops.syncretism.io/api.html
+
 resp = requests.get(url=url, data = userin, headers= {'Content-Type': 'application/json'})
 data = resp.json() 
 
